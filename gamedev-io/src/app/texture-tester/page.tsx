bc2c29e64d6892e3bc187a6c861514b6f4f37b93
NjrexUI <nurakhmet.aidarbek@gmail.com>
2025-10-19 09:25:00 +0500

    directoty name change

--- gamedev-io/src/app/texture-tester/page.tsx
+++ gamedev-io/src/app/texture-tester/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react';
 import TextureTesterClient from './TextureTesterClient';
 
+export const dynamic = 'force-dynamic';
+
 export default function TextureTesterPage() {
 
   return (
